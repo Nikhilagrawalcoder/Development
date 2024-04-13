f71b5dfb73e4278406a891c5b10249e72ee78609
NIK0605 <nikhilagrawal0605@gmail.com>
2024-04-13 19:51:44 +0530

    add

---
+++ Guess the Number/script.js
@@ -0,0 +1,62 @@
+'use strict';
+let secretNumber=Math.floor(Math.random()*20)+1;
+let highScore=0;
+let score=20;
+document.querySelector('.check').addEventListener('click',function(){
+    const guess= Number(document.querySelector('.guess').value);
+    if(!guess){
+        document.querySelector('.message').textContent='No Number!';
+    }
+   else if(guess===secretNumber){
+    document.querySelector('.message').textContent="Correct Number"
+    document.querySelector('body').style.backgroundColor="#60b347";
+    document.querySelector('.number').style.width="30rem"
+document.querySelector('.number').textContent=secretNumber;
+if (score > highScore) {
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+  }
+   }
+   else if(guess>secretNumber){
+    if(score>0){
+        document.querySelector('.message').textContent="Too High";
+        score--;
+        document.querySelector('.score').textContent=score;
+    }
+    else if(score==0){
+        document.querySelector('.message').textContent="You Lost the game"
+    }
+   
+    
+   }
+   else if(guess<secretNumber){
+    if(score>0){
+        document.querySelector('.message').textContent="Too Low";
+    score--;
+    document.querySelector('.score').textContent=score;
+    }
+    else if(score==0){
+        document.querySelector('.message').textContent="You Lost the game"
+    }
+   
+   }
+    
+    
+})
+
+document.querySelector('.again').addEventListener('click',function(){
+    score=20;
+    secretNumber=Math.floor(Math.random()*20)+1;
+    document.querySelector('.message').textContent="Start guessing.. ";
+    document.querySelector('body').style.backgroundColor="#222";
+    document.querySelector('.number').style.width="15rem"
+// document.querySelector('.number').textContent=secretNumber;
+highScore=0;
+document.querySelector('.score').textContent=score;
+document.querySelector('.number').textContent="?";
+document.querySelector('.guess').value='';
+
+
+})
+// document.querySelector('.highscore').textContent=highScore;
+// console.log(highScore);
